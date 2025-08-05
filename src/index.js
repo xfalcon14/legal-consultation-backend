3a7cb9533412fa2d975ace1e1a1df1e5a7947f67
xfalcon14 <65176391+xfalcon14@users.noreply.github.com>
2025-08-05 22:47:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -94,4 +94,6 @@ app.get('/balance', auth('consultant'), async (req, res) => {
     res.json({ balance: user.balance });
 });
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+
